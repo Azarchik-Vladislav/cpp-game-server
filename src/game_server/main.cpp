c7a4bc05fb3a362ebd7a94f47b10e7a548027fbb
Azarchik-Vladislav <v.azarchik@mail.ru>
2025-09-13 14:29:37 +0500

    Pravki

--- src/game_server/main.cpp
+++ src/game_server/main.cpp
@@ -30,7 +30,7 @@ namespace {
 template <typename Fn>
 void RunWorkers(unsigned n, const Fn& fn) {
     n = std::max(1u, n);
-    std::vector<std::jthread> workers;
+    std::vector<std::thread> workers;
     workers.reserve(n - 1);
     // Запускаем n-1 рабочих потоков, выполняющих функцию fn
     while (--n) {
